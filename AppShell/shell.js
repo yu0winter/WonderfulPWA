abc44b3f0a0a0a33e25a3daf0f67a266571c9383
牛玉龙 <niuyulong@jd.com>
2018-04-20 18:26:36 +0800

    添加AppIcon，注册ServiceWorker

--- AppShell/shell.js
+++ AppShell/shell.js
@@ -5,15 +5,13 @@
  *
  **/
 
-
 var indexUrls = ['/weather/index.html', '/news/index.html', '/other/index.html'];
-
 var btns = [].slice.call(document.getElementsByClassName("toolbar__Item__Button"));
 btns.forEach(function(v, i) { //v==value　为arr项，i==index　为arr索引
 
     v.onclick = function() {
 
-      	var pathname = window.location.pathname;
+        var pathname = window.location.pathname;
         var target = indexUrls[this.getAttribute('tag')];
 
         if (pathname != target) {
@@ -22,3 +20,33 @@ btns.forEach(function(v, i) { //v==value　为arr项，i==index　为arr索引
         console.log(target);
     };
 });
+
+/*
+ *	注册serviceWorker
+ */
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+/*
+ *	当执行”添加到主屏幕“的操作时，内部会触发相应的事件beforinstallprompt
+ */
+window.addEventListener('beforeinstallprompt', function(e) {
+    e.userChoice.then(function(result) {
+        if (result.outcome == 'dismissed') {
+            // 发送数据进行分析
+            // 用户取消了
+        } else {
+            // 发送数据进行分析
+            // 用户注册了
+        }
+    })
+})
