d84496ab077919ccb87c365d7135913b968efa1f
牛玉龙 <niuyulong@jd.com>
2018-04-20 20:42:24 +0800

    引入ServiceWorker并调通。

--- AppShell/shell.js
+++ AppShell/shell.js
@@ -20,33 +20,3 @@ btns.forEach(function(v, i) { //v==value　为arr项，i==index　为arr索引
         console.log(target);
     };
 });
-
-/*
- *	注册serviceWorker
- */
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('sw.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }).catch(function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
-
-/*
- *	当执行”添加到主屏幕“的操作时，内部会触发相应的事件beforinstallprompt
- */
-window.addEventListener('beforeinstallprompt', function(e) {
-    e.userChoice.then(function(result) {
-        if (result.outcome == 'dismissed') {
-            // 发送数据进行分析
-            // 用户取消了
-        } else {
-            // 发送数据进行分析
-            // 用户注册了
-        }
-    })
-})
--- sw.js
+++ sw.js
@@ -6,154 +6,158 @@ const
   version = '1.0.0',
   CACHE = version + '::PWAsite',
   offlineURL = '/offline/',
+  // 缓存必要的资源
   installFilesEssential = [
     '/',
     '/manifest.json',
-    '/css/styles.css',
-    '/js/main.js',
-    '/js/offlinepage.js',
-    '/images/logo/logo152.png'
+    '/AppShell/inline.css',
+    '/scripts/main.js',
+    '/images/launching.jpeg'
   ].concat(offlineURL),
+  // 描述性资源，比如logo等
   installFilesDesirable = [
-    '/favicon.ico',
-    '/images/logo/logo016.png',
-    '/images/hero/power-pv.jpg',
-    '/images/hero/power-lo.jpg',
-    '/images/hero/power-hi.jpg'
+    '/images/icons/icon-72x72.png',
+    '/images/icons/icon-128x128.png',
+    '/images/icons/icon-144x144.png',
+    '/images/icons/icon-152x152.png',
+    '/images/icons/icon-192x192.png',
+    '/images/icons/icon-384x384.png',
+    '/images/icons/icon-512x512.png'
   ];
 
 // install static assets
+// 安装全局静态资源
 function installStaticFiles() {
 
   return caches.open(CACHE)
     .then(cache => {
 
-      // cache desirable files
+      // cache desirable files 
       cache.addAll(installFilesDesirable);
 
-      // cache essential files
+      // cache essential files 
       return cache.addAll(installFilesEssential);
 
     });
 
 }
 
-// clear old caches
-function clearOldCaches() {
+// // clear old caches
+// function clearOldCaches() {
 
-  return caches.keys()
-    .then(keylist => {
+//   return caches.keys()
+//     .then(keylist => {
 
-      return Promise.all(
-        keylist
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      );
+//       return Promise.all(
+//         keylist
+//           .filter(key => key !== CACHE)
+//           .map(key => caches.delete(key))
+//       );
 
-    });
+//     });
 
-}
+// }
 
-// application installation
-self.addEventListener('install', event => {
+// // application installation
+// self.addEventListener('install', event => {
 
-  console.log('service worker: install');
+//   console.log('service worker: install');
 
-  // cache core files
-  event.waitUntil(
-    installStaticFiles()
-    .then(() => self.skipWaiting())
-  );
+//   // cache core files
+//   event.waitUntil(
+//     installStaticFiles()
+//     .then(() => self.skipWaiting())
+//   );
 
-});
-
-
-// application activated
-self.addEventListener('activate', event => {
-
-  console.log('service worker: activate');
-
-	// delete old caches
-  event.waitUntil(
-    clearOldCaches()
-    .then(() => self.clients.claim())
-	);
-
-});
+// });
 
 
-// is image URL?
-let iExt = ['png', 'jpg', 'jpeg', 'gif', 'webp', 'bmp'].map(f => '.' + f);
-function isImage(url) {
+// // application activated
+// self.addEventListener('activate', event => {
 
-  return iExt.reduce((ret, ext) => ret || url.endsWith(ext), false);
+//   console.log('service worker: activate');
 
-}
+// 	// delete old caches
+//   event.waitUntil(
+//     clearOldCaches()
+//     .then(() => self.clients.claim())
+// 	);
 
-
-// return offline asset
-function offlineAsset(url) {
-
-  if (isImage(url)) {
-
-    // return image
-    return new Response(
-      '<svg role="img" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title>offline</title><path d="M0 0h400v300H0z" fill="#eee" /><text x="200" y="150" text-anchor="middle" dominant-baseline="middle" font-family="sans-serif" font-size="50" fill="#ccc">offline</text></svg>',
-      { headers: {
-        'Content-Type': 'image/svg+xml',
-        'Cache-Control': 'no-store'
-      }}
-    );
-
-  }
-  else {
-
-    // return page
-    return caches.match(offlineURL);
-
-  }
-
-}
+// });
 
 
-// application fetch network data
-self.addEventListener('fetch', event => {
+// // is image URL?
+// let iExt = ['png', 'jpg', 'jpeg', 'gif', 'webp', 'bmp'].map(f => '.' + f);
+// function isImage(url) {
 
-  // abandon non-GET requests
-  if (event.request.method !== 'GET') return;
+//   return iExt.reduce((ret, ext) => ret || url.endsWith(ext), false);
 
-  let url = event.request.url;
+// }
 
-  event.respondWith(
 
-    caches.open(CACHE)
-      .then(cache => {
+// // return offline asset
+// function offlineAsset(url) {
 
-        return cache.match(event.request)
-          .then(response => {
+//   if (isImage(url)) {
 
-            if (response) {
-              // return cached file
-              console.log('cache fetch: ' + url);
-              return response;
-            }
+//     // return image
+//     return new Response(
+//       '<svg role="img" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title>offline</title><path d="M0 0h400v300H0z" fill="#eee" /><text x="200" y="150" text-anchor="middle" dominant-baseline="middle" font-family="sans-serif" font-size="50" fill="#ccc">offline</text></svg>',
+//       { headers: {
+//         'Content-Type': 'image/svg+xml',
+//         'Cache-Control': 'no-store'
+//       }}
+//     );
 
-            // make network request
-            return fetch(event.request)
-              .then(newreq => {
+//   }
+//   else {
 
-                console.log('network fetch: ' + url);
-                if (newreq.ok) cache.put(event.request, newreq.clone());
-                return newreq;
+//     // return page
+//     return caches.match(offlineURL);
 
-              })
-              // app is offline
-              .catch(() => offlineAsset(url));
+//   }
 
-          });
+// }
 
-      })
 
-  );
+// // application fetch network data
+// self.addEventListener('fetch', event => {
 
-});
+//   // abandon non-GET requests
+//   if (event.request.method !== 'GET') return;
+
+//   let url = event.request.url;
+
+//   event.respondWith(
+
+//     caches.open(CACHE)
+//       .then(cache => {
+
+//         return cache.match(event.request)
+//           .then(response => {
+
+//             if (response) {
+//               // return cached file
+//               console.log('cache fetch: ' + url);
+//               return response;
+//             }
+
+//             // make network request
+//             return fetch(event.request)
+//               .then(newreq => {
+
+//                 console.log('network fetch: ' + url);
+//                 if (newreq.ok) cache.put(event.request, newreq.clone());
+//                 return newreq;
+
+//               })
+//               // app is offline
+//               .catch(() => offlineAsset(url));
+
+//           });
+
+//       })
+
+//   );
+
+// });
