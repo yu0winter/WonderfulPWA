0373983e489b9928a5bc50b6601557697f175bc0
NiuYulong <15011116026@163.com>
2018-04-22 20:14:00 +0800

    三个页面简单功能实现

--- news/news.js
+++ news/news.js
@@ -1,4 +1,3 @@
-
 // NEWAPI 网站秘钥
 // 账号 niuyulong1991@gmail.com
 const Appkey = "64a2b432ba4e475091d80f3de19937b9";
@@ -6,27 +5,30 @@ const main = document.querySelector('main');
 const sourceSelector = document.querySelector('#sourceSelector');
 const defaultSource = 'Time';
 
-window.addEventListener('load',e => {
-//	updateNews();
-//	updateSources();
-	sourceSelector.addEventListener('change', evt => updateNews(evt.target.value));
-	updateSources().then(() => {
-    	sourceSelector.value = defaultSource;
-    updateNews();
-  });
+window.addEventListener('load', e => {
+    //	updateNews();
+    //	updateSources();
+    sourceSelector.addEventListener('change', evt => updateNews(evt.target.value));
+    var loader = document.getElementsByClassName('loader')[0];
+    loader.style.display = '';
+    updateSources().then(() => {
+        sourceSelector.value = defaultSource;
+        updateNews();
+	    loader.style.display = 'none';
+    });
 });
 
 window.addEventListener('online', () => updateNews(sourceSelector.value));
 
-async function updateNews (source = defaultSource) {
-	const res = await 
-	fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${Appkey}&sortBy=top`);
-	const json = await res.json();
-	main.innerHTML = json.articles.map(createArticle).join('\n');
+async function updateNews(source = defaultSource) {
+    const res = await
+    fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${Appkey}&sortBy=top`);
+    const json = await res.json();
+    main.innerHTML = json.articles.map(createArticle).join('\n');
 }
 
 function createArticle(article) {
-	return `
+    return `
     <div class="article">
       <a href="${article.url}">
         <h2>${article.title}</h2>
@@ -38,9 +40,9 @@ function createArticle(article) {
 }
 
 async function updateSources() {
-	main.innerHTML = '';
-	const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${Appkey}`); 
-	const json = await res.json();
-	sourceSelector.innerHTML = json.sources
-	.map(src => `<option value="${src.id}">${src.name}</option>`).join('\n');
+    main.innerHTML = '';
+    const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${Appkey}`);
+    const json = await res.json();
+    sourceSelector.innerHTML = json.sources
+        .map(src => `<option value="${src.id}">${src.name}</option>`).join('\n');
 }
